5e7f509fe3ebfefb1de0024d329bea55f3d9dfb3
soloking44 <solop8483@gmail.com>
2023-11-28 11:29:14 -0800

    program that prints the alphabet

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    char = i
-    if i % 2 != 0:
-        char = char - 32
-    print(chr(char), end="")
-
+for k in range(122, 96, -1):
+    if k % 2 == 0:
+        j = chr(k)
+    else:
+        j = chr(k-32)
+    print("{}".format(j), end="")
