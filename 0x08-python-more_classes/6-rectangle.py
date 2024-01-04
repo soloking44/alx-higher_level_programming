b0b31ccb7ca4fea8219b34d8c94609cd6bbcc5df
soloking44 <solop8483@gmail.com>
2024-01-04 04:26:33 -0800

    6-rectangle

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,11 +1,10 @@
-
 #!/usr/bin/python3
-# 6-rectangle.py
 """Defines a Rectangle class."""
 
 
 class Rectangle:
     """Represent a rectangle.
+
     Attributes:
         number_of_instances (int): The number of Rectangle instances.
     """
@@ -14,6 +13,7 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
+
         Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
@@ -60,15 +60,16 @@ class Rectangle:
 
     def __str__(self):
         """Return the printable representation of the Rectangle.
+
         Represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
             return ("")
 
         rect = []
-        for x in range(self.__height):
-            [rect.append('#') for y in range(self.__width)]
-            if x != self.__height - 1:
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
 
