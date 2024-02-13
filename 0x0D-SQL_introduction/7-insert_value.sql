45049eaf2225801350714d3d28318da985798fbe
soloking44 <solop8483@gmail.com>
2024-02-13 07:31:07 -0800

    7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- code that shows the count of entries where the id equals 89 in the table "first_table"
+-- code that shows the number of entries where id = 89 in the table first_table
 
 SELECT count(*) FROM first_table WHERE id=89;
