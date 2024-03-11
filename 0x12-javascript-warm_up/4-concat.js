c1eaf06461d0eb1521dcff2b7860e4c1c632999f
soloking44 <solop8483@gmail.com>
2024-03-11 04:35:01 -0700

    4-concat

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+console.log(firstArg + ' is ' + secondArg);
+// It shows 2 arguments given, in this order: “ is ”
