8bc6c351e70167fa11e145b7939e991382296244
soloking44 <solop8483@gmail.com>
2024-03-11 06:06:39 -0700

    10-factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (n === 0 || isNaN(n)) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+let firstArg = process.argv[2];
+firstArg = parseInt(firstArg);
+console.log(factorial(firstArg));
+// It calculates and displays the factorial
