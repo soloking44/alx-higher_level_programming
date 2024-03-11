550ef24f99b857b6d7481a169a392ab55d0324d5
soloking44 <solop8483@gmail.com>
2024-03-11 04:52:14 -0700

    6-multi_languages_loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const arrString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < arrString.length; i++) {
+  console.log(arrString[i]);
+}
+// It shows 3 lines: (in this way 1-multi_languages.js)
+// Employ an array of string and iterate with a loop
