b333cefef495fb4c87d4684fb5d7a88007f4fb93
soloking44 <solop8483@gmail.com>
2024-03-11 07:09:12 -0700

    13-add

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,14 +1,5 @@
 #!/usr/bin/node
-const myObject = {
-  type: 'object',
-  value: 12
-};
-console.log(myObject);
-myObject.value = 89;
-console.log(myObject);
-
-#!/usr/bin/node
 exports.add = function (x, m) {
   return x + m;
 };
-// It computes the sum of two integers.
+// Computes the sum of two integers.
