e216fa3f8f9dec137fb8c14abbd71123730ea6a4
soloking44 <solop8483@gmail.com>
2024-03-15 00:06:19 -0700

    2-my_filter_states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,17 +5,17 @@ A code is needed to accept an argument and show all entries within
 """
 
 import MySQLdb
-from sys import argv
+import sys
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    query = "SELECT * FROM states\
-             WHERE states.name LIKE BINARY '{}'\
-             ORDER BY states.id ASC".format(sys.argv[4])
-    cursor = db.cursor()
-    cursor.execute(query)
-    for state in cursor.fetchall():
-        print(state)
-    cursor.close()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
     db.close()
