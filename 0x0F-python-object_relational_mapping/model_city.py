0727384df99bb6b4ecab25d525a6c0b07a5bf614
soloking44 <solop8483@gmail.com>
2024-03-16 11:32:32 -0700

    model_city

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""It has an explanation of a State with class instance
-Base = declarative_base() """
-
-
-from sqlalchemy import Column, Integer, String
+"""
+It has the explanation of the city within a class.
+"""
 from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import ForeignKey
 
 
 class City(Base):
-    """ This city is fetch within the database and 
-    connects class to a state table.
-    Attr:
-        id, name
     """
-__tablename__ = 'cities'
+    It explains all city within a class.
+    """
+    __tablename__ = 'cities'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
