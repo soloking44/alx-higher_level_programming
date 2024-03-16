3830a844186bb4c48957c23aa4a8c7c1eae6c2b5
soloking44 <solop8483@gmail.com>
2024-03-16 11:20:38 -0700

    14-model_city_fetch_by_state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This a code which shows every item within the database.
+"""A code which shows each item within the database.
 """
 import sys
 from model_state import Base, State
@@ -8,11 +8,15 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in (session.query(State.name, City.id, City.name)
-                     .filter(State.id == City.state_id)):
-        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2])
+
+    query = session.query(State, City).join(City, State.id == City.state_id).\
+        order_by(City.id)
+    for state, city in query:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    session.close()
