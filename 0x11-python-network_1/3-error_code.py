59f74be5b95d2f78df6eed415bb2218232cebc23
soloking44 <solop8483@gmail.com>
2024-03-28 06:31:22 -0700

    3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This code to:
+- Gets a URL,
+- Transmites a request into URL
+- Shows the body of the outcome (decoded in utf-8).
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
+
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
