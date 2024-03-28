c91e9df5d7d01b5f112a3a8a32799a5ac4eca3b2
soloking44 <solop8483@gmail.com>
2024-03-28 08:24:04 -0700

    100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This is code to accept two data so as to solve this task.
+
+Usage: ./100-github_commits.py <repository name> <repository owner>
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
