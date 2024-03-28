ad4d07f0ec42ead022498cc881b04c4be1c35aff
soloking44 <solop8483@gmail.com>
2024-03-28 06:22:06 -0700

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This code to:
+- Gets a URL,
+- Transmites POST request into the URL
+- Fetches email as data
+- Shows the body of the outcome
+"""
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
